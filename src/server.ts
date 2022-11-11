964cb269318d5c365ea133edd05ff730461e87b3
Renato Marino Henz <renatomarinohenz@gmail.com>
2022-11-10 22:21:37 -0300

    Creation of models, seed file and new routes.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors';
+import { z } from 'zod';
 import { PrismaClient } from '@prisma/client';
+import ShortUniqueId from 'short-unique-id';
 
 // Creating connection on Prisma
 const prisma = new PrismaClient({
@@ -18,7 +20,21 @@ async function bootstrap() {
         origin: true,
     });
 
-    // Creating the oute to count the number of created pools
+    // Creating the route to count the number of users
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count();
+
+        return { count };
+    });
+
+    // Creating the route to count the number of guesses
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count();
+
+        return { count };
+    });
+    
+    // Creating the route to count the number of created pools
     fastify.get('/pools/count', async () => {
         // const pools = await prisma.pool.findMany({
         //     where: {
@@ -30,7 +46,28 @@ async function bootstrap() {
         const count = await prisma.pool.count();
 
         return { count };
-    })
+    });
+
+    // Route to create a new pool
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        const { title } = createPoolBody.parse(request.body);
+
+        const generate = new ShortUniqueId({ length: 6 })
+        const code = String(generate()).toUpperCase();
+
+        await prisma.pool.create({
+            data: {
+                title, 
+                code,
+            }
+        })
+
+        return reply.status(201).send({ code });
+    });
 
     // Setting port and host
     await fastify.listen({ port: 3333, host: '0.0.0.0' });
